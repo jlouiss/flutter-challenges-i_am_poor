555f3011abe217b047abfa9a61c3f6e69434087c
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-07-29 21:21:26 +0100

    Create MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,25 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber[800],
+          title: Text('I am poor'),
+        ),
+        backgroundColor: Colors.amberAccent[100],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/head.png'),
+          ),
+        ),
+      ),
+    );
+  }
 }
